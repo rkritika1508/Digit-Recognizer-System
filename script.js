e414c571a384d0b52a3d56b82777fe062622cba2
Kritika <rkritika1508@gmail.com>
2019-04-03 02:57:46 +0530

    Made modifications

--- script.js
+++ script.js
@@ -49,3 +49,12 @@ function addClick(x, y, dragging) {
 	clickY.push(y);
 	clickDrag.push(dragging);
 }
+
+
+//References:
+// https://www.html5canvastutorials.com/labs/html5-canvas-paint-application/
+// http://codetheory.in/creating-a-paint-application-with-html5-canvas/
+// https://enlight.nyc/projects/web-paint/
+// http://www.primaryobjects.com/2011/10/05/creating-a-simple-paint-app-with-html5-canvas-and-javascript/
+// http://www.williammalone.com/articles/create-html5-canvas-javascript-drawing-app/
+// https://code.tutsplus.com/tutorials/how-to-create-a-web-based-drawing-application-using-canvas--net-14288
